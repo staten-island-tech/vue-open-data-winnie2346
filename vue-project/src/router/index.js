699df42928c99501572e690b5fd95f9f2013effb
winnie2346 <winniedong68@gmail.com>
2026-03-16 12:32:28 -0400

    xczcczc

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import rat from '@/views/rat.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/chi',
+      name: 'rat',
+      component: rat,
+    },
+  ],
 })
 
 export default router
---
+++ vue-project/src/views/rat.vue
@@ -0,0 +1,22 @@
+<template>
+  <div><h1>dosomething</h1></div>
+</template>
+
+<script setup>
+// import { ref, onMounted } from 'vue'
+// const ratstuff = ref([])
+// async function getthemrats() {
+//   try {
+//     const calehenituse = await fetch('https://data.cityofnewyork.us/resource/p937-wjvj.json')
+//     const alvercrossman = await calehenituse.json()
+//     ratstuff.value = alvercrossman.results
+//   } catch (error) {
+//     console.log(chickenbutt)
+//   }
+// }
+// onMounted(() => {
+//   getthemrats()
+// })
+</script>
+
+<style scoped></style>
