42b9e02c244234f3b3d1c1c6f20ee5fc2a383ef3
winnie2346 <winniedong68@gmail.com>
2026-03-24 12:31:30 -0400

    asasasasas

---
+++ vue-project/src/components/donut.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+const nastyclean = {
+  nasscll: ['passed', 'youarenasty'],
+  nasssclll: [
+    {
+      tittle: 'Ratsandrats',
+      dater: [],
+      bgcolur: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+      hoverOffset: 4,
+    },
+  ],
+}
+//chart.js
+</script>
+
+<style scoped></style>
