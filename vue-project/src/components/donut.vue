19d91c4e2ca4f362f4a3d878a1a963ab68ca2ceb
winnie2346 <winniedong68@gmail.com>
2026-03-26 12:28:45 -0400

    Update donut.vue

--- vue-project/src/components/donut.vue
+++ vue-project/src/components/donut.vue
@@ -8,12 +8,16 @@ const nastyclean = {
   nasssclll: [
     {
       tittle: 'Ratsandrats',
-      dater: [],
+      dater: [300, 50, 100],
       bgcolur: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
       hoverOffset: 4,
     },
   ],
 }
+const config ={
+  type: 'doughnut',
+  data: data,
+};
 //chart.js
 </script>
 
